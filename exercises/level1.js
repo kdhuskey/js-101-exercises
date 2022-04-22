bb03e6957a6b3a510ece0a6ba9346ba4c6bb323f
Kyle Huskey <kylehuskey@gmail.com>
2022-04-22 16:54:31 -0400

    completed hw

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,46 @@
 function lvl1exercise1 () {
   // Declare a variable as null and return it!
+  const variableName = null
+  return variableName
 
 }
 
 function lvl1exercise2 () {
   // Declare a variable with a number value and return it
+  const numberVariable = 5
+  return numberVariable
 
 }
 
 function lvl1exercise3 () {
   // Declare a variable with a floating point number value that is not a whole number and return it
-
+const floatNum = 3.141592654
+return floatNum
 }
 
 function lvl1exercise4 () {
   // Declare a variable and with a string value as "Hello World!" and return it
-
+const greeting = 'Hello World!'
+return greeting
 }
 
 function lvl1exercise5 () {
   // Declare a variable and with an array value containing the string "Hello World!" and the number 4 and return it
-
+const level5 = ['Hello World!', 4]
+return level5
 }
 
 function lvl1exercise6 () {
   // Declare a variable as an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+const level6 = {
+  key1: 'Hello World!',
+  key2: 4
+}
+return level6
 }
 
 function lvl1exercise7 () {
   // Declare a variable as a boolean value 'false' and return it
-
+const level7 = false
+return level7
 }
--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,32 @@
 function lvl3exercise1 () {
   // Create strings "hello" and a "world", return the concatenation of the two
-
+const greet = 'hello'
+const ing = 'world'
+return `${greet}${ing}`
 }
 
 function lvl3exercise2 () {
   // Create a "hello" and a 12, return the concatenation of the two
-
+const greet = 'hello'
+const num = 12
+return `${greet}${num}`
 }
 
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+const num = 12
+return `${num}`
 }
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
+  const greeting = "hello world!"
+  return greeting.length
 
 }
 
 function lvl3exercise5 () {
   // Create a "hello world!" string. Return the index of the word "world".
-
+const level3 = 'hello world!'
+return level3.indexOf("world")
 }
